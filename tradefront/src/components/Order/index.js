de985cf5fa92d4c8bab2f2ff08231c0f9daca445
hiddenbebb <cbj4133@naver.com>
2024-07-09 00:20:46 +0900

    coindata 오류 해결

--- tradefront/src/components/Order/index.js
+++ tradefront/src/components/Order/index.js
@@ -3,7 +3,7 @@ import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import OrderBuy from "./index.buy";
 import style from "./style/style.module.css";
-import { fetchAccount, fetchPrice } from "api/Upbit/Upbit_api";
+import { fetchPrice } from "api/Upbit/Upbit_api";
 
 const Order = ({ market }) => {
   const [coinData, setCoinData] = useState(null);
@@ -11,13 +11,15 @@ const Order = ({ market }) => {
 
   useEffect(() => {
     const getData = async () => {
+      console.log(market);
       const data = await fetchPrice(market);
+      console.log(data);
       if (data) {
         setCoinData(data[0]);
       }
     };
     getData();
-  }, []);
+  }, [market]);
 
   return (
     <div className={style.orderParent}>
@@ -27,7 +29,7 @@ const Order = ({ market }) => {
         className="mb-3"
       >
         <Tab eventKey="buy" title="매수">
-          <OrderBuy coin={coinData} />
+          {coinData ? <OrderBuy coin={coinData} /> : <div>Loading...</div>}
         </Tab>
         <Tab eventKey="sell" title="매도">
           Tab content for Profile
