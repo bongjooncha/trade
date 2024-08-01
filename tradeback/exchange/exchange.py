d80c6a6f29399340893e99c61e428a7ebdb2904e
hiddenbebb <cbj4133@naver.com>
2024-08-01 18:11:06 +0900

    back 수정

--- tradeback/exchange/exchange.py
+++ tradeback/exchange/exchange.py
@@ -4,13 +4,28 @@ from models import get_db_connection
 exchange_api = Blueprint('exchange',__name__)
 
 #평균가격 확인
-@exchange_api.route("average")
+@exchange_api.route("/average", methods=['POST'])
 def exchange_average():
-    # print(country)
+    country = request.json['country']
     conn = get_db_connection("AWS")
     try:
         with conn.cursor() as cursor:
-            cursor.execute(f"SELECT AVG(Close) FROM KRWUSD")
+            cursor.execute(f"SELECT AVG(Close) FROM {country}")
+            result = cursor.fetchall()
+            return jsonify(result)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+    finally:
+        conn.close()
+
+#전체 Close price 받기
+@exchange_api.route("/price", methods=['POST'])
+def exchange_price():
+    country = request.json['country']
+    conn = get_db_connection("AWS")
+    try:
+        with conn.cursor() as cursor:
+            cursor.execute(f"SELECT Date,Close FROM {country}")
             result = cursor.fetchall()
             return jsonify(result)
     except Exception as e:
