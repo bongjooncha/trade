1a8947c59906e2530a45d9aedff860f4013bbf12
hiddenbebb <cbj4133@naver.com>
2024-08-07 00:55:06 +0900

    index page 수정

--- tradefront/src/pages/Home/IndexChart/index.jsx
+++ tradefront/src/pages/Home/IndexChart/index.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useMemo } from "react";
 import { fetchIndexAverage, fetchIndexPrice } from "api/Flow/Index_api";
 import styles from "./style/exchangehart.module.css";
 import { TotalIndex } from "./index.utils";
@@ -9,11 +9,16 @@ import Chart from "./index.chart";
 const IndexChart = () => {
   const [selectedIndex, setSelectedIndex] = useState(["^GSPC"]);
   const [chartData, setChartData] = useState([]);
+  const [fetchedIndexes, setFetchedIndexes] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
+      const newIndexes = selectedIndex.filter(
+        (index) => !fetchedIndexes.includes(index)
+      );
+
       const sericesData = await Promise.all(
-        selectedIndex.map(async (index) => {
+        newIndexes.map(async (index) => {
           try {
             const { name } = TotalIndex.find((item) => item.ticker === index);
             const priceResponse = await fetchIndexPrice(index);
@@ -30,17 +35,39 @@ const IndexChart = () => {
           }
         })
       );
-
-      setChartData(sericesData.filter((series) => series !== null)); // null 값 제거
+      const newData = sericesData.reduce((acc, series) => {
+        if (series) {
+          acc[series.name] = series.data;
+        }
+        return acc;
+      }, {});
+      setChartData((prevData) => ({ ...prevData, ...newData }));
+      setFetchedIndexes((prevIndexes) => [...prevIndexes, ...newIndexes]);
     };
     fetchData();
   }, [selectedIndex]);
+
+  const filteredChartData = useMemo(() => {
+    const selectedNames = selectedIndex
+      .map((ticker) => {
+        const item = TotalIndex.find((item) => item.ticker === ticker);
+        return item ? item.name : null;
+      })
+      .filter((name) => name !== null);
+
+    return Object.keys(chartData)
+      .filter((key) => selectedNames.includes(key))
+      .map((key) => ({
+        name: key,
+        data: chartData[key],
+      }));
+  }, [chartData, selectedIndex]);
 
   return (
     <div>
       <div className={styles.index}>
         <div>
-          <Chart data={chartData} />
+          <Chart data={filteredChartData} />
         </div>
         <div className={styles.indexButton}>
           <IndexButton
